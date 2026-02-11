fc1f0984204402a95d37e12e905ef9dafefc4a25
Devin Johnson <devin.johnson@noaa.gov>
2026-02-11 10:39:26 -0700

    add stage II calcs

---
+++ testing/turtle/_2_parallel_prediction.R
@@ -0,0 +1,118 @@
+library(nimble)
+library(nimbleEcology)
+library(nimbleJSextras)
+library(tidyverse)
+library(coda)
+library(foreach)
+library(doFuture)
+
+### Read in MCMC sample from stage I
+
+load("honu_mcmc_sample.RData")
+par_mcmc_list <- samples_list[c("xi","phi","theta","p","nu")]
+honu_ch <- as.matrix(honu_ch)
+
+# Define number of cores and chunk size
+n_cores <- 10 #parallel::detectCores() - 2
+total_iters <- dim(par_mcmc_list$xi)[1]
+chunk_indices <- split(1:total_iters, cut(1:total_iters, n_cores))
+
+# Raise the limit for the transfer
+# options(future.globals.maxSize = 1500 * 1024^2)
+
+# Create parallel sessions
+plan(multisession) # Use multicore on Mac/Linux for faster shared memory
+seeds <- 41 + 1:length(chunk_indices)
+
+st <- Sys.time()
+
+results <- foreach(j = 1:length(chunk_indices),
+  .options.future = list(
+    seed = TRUE,
+    packages = c("nimble","nimbleEcology","nimbleJSextras"),
+    globals=c("par_mcmc_list","honu_ch","chunk_indices","seeds")
+    )
+) %dofuture% {
+  # Process a 'chunk' of iterations inside the worker
+  # We subset the input matrices inside the worker to save memory
+  set.seed(seeds[j])
+  source("make_hmm_mats.R")
+  source("predict_abundance.R")
+  idx <- chunk_indices[[j]]
+  worker_pred <- compileNimble(predict_abundance)
+  pred <- worker_pred(
+    xi = par_mcmc_list$xi[idx, , drop = FALSE],
+    phi = par_mcmc_list$phi[idx, , drop = FALSE],
+    theta = par_mcmc_list$theta[idx],
+    p = par_mcmc_list$p[idx, , drop = FALSE],
+    nd = 7,
+    nu = par_mcmc_list$nu[idx],
+    x = honu_ch
+  )
+  pred$N_avail
+
+}
+
+et <- Sys.time()
+et-st
+
+plan("sequential")
+
+# Extract and stack N_avail
+N_avail <- do.call(rbind, results)[,-c(1:10)] %>% mcmc()
+
+###
+
+# Yearly abundance of reproductive females
+Ndf <- data.frame(year = 1998:2018, est=round(colMeans(N_avail)), hpd = HPDinterval(N_avail))
+ggplot(Ndf) + geom_point(aes(x=year, y=est), size=3) +
+  geom_errorbar(aes(x=year, ymin=hpd.lower, ymax=hpd.upper), width=0.2) +
+  geom_path(aes(x=year, y=est)) + ylab("Abundance") + xlab("Year") +
+  theme_bw()
+# ggsave("honu_Nt.pdf", width=6.5, height=4)
+get_trend <- function(x){
+  L <- length(x)
+  fit <- lm(log(x) ~ c(1:L))
+  return((exp(fit$coefficients[2])-1)*100)
+}
+ttt <- mcmc(apply(N_avail[,1:15], 1, get_trend))
+
+# Renest dist
+theta <- samples_list$theta
+rn_dist <- NULL
+for(i in 1:length(theta)){
+  rn_dist <- rbind(rn_dist, dpois(c(0:9)-1, theta[i]))
+}
+rn_dist <- mcmc(rn_dist)
+rndf <- data.frame(year=c(1:10), est=colMeans(rn_dist), hpd = HPDinterval(rn_dist))
+ggplot(rndf) + geom_point(aes(x=year, y=est), size=3) +
+  geom_errorbar(aes(x=year, ymin=hpd.lower, ymax=hpd.upper), width=0.2) +
+  geom_path(aes(x=year, y=est)) + ylab("Renest probability") + xlab("Year") +
+  theme_bw()
+
+mean(theta+2)
+HPDinterval(mcmc(theta+2))
+
+
+plot(samples_list$Nsuper)
+summary(samples_list$Nsuper)
+plot(x=c(1994:2023), colMeans(samples_list$N))
+plot(x=c(1994:2023), colMeans(samples_list$N_nest))
+
+summary(samples_list$N)
+
+xi <- samples_list$xi
+xi <- sweep(xi,1,rowSums(xi),"/")
+plot(colMeans(xi))
+
+plot(samples_list$pstar)
+
+summary(samples_list$phi)
+
+summary(samples_list$p)
+plot(colMeans(samples_list$p))
+plot(samples_list$sig_tau)
+
+plot(samples_list$theta+2)
+
+
--- testing/turtle/predict_abundance.R
+++ testing/turtle/predict_abundance.R
@@ -1,39 +1,85 @@
+
+predlist <- nimbleList(
+  N_nest = double(2),
+  N_avail = double(2)
+)
+
+# debuglist <- nimbleList(
+#   zeta = double(1),
+#   pi = double(1),
+#   Gamma = double(3),
+#   Pmats = double(3),
+#   nu_t = double(2)
+# )
+
 predict_abundance <- nimbleFunction(
   run = function(xi=double(2), phi = double(2), theta=double(1), p=double(2),
-                 nd=integer(), nu = integer(1), x = integer(2)) {
+                 nd=integer(0), nu = double(1), x = double(2)) {
     iter <- dim(xi)[1]
     K <- dim(xi)[2]
     n <- dim(x)[1]
-    n_states <- 3+ nd
+    n_states <- 3+nd
 
-    x_tilde <- numeric(K)
+    xi_tilde <- numeric(K)
     Gamma <- nimArray(dim=c(n_states,n_states,K-1))
     pi <- numeric(n_states)
     Pmats <- nimArray(dim=c(n_states,2,K))
-
+    nu_t <- nimMatrix(nrow=n_states, ncol=K)
+    det_state <- nimMatrix(nrow=n, ncol = K)
+    nest_obs <- nimMatrix(nrow=n, ncol = K)
+    avail_obs <- nimMatrix(nrow=n, ncol = K)
+    N_nest <- nimMatrix(nrow=iter, ncol=K)
+    N_avail <- nimMatrix(nrow=iter, ncol=K)
+    zeta <- numeric(nd)
 
     for(j in 1:iter){
 
       ### Load HMM matrices
+      zeta <- make_dt_pois(nd, theta[j], shift=1)
       for(t in 1:(K-1)){
         xi_tilde[t] <- xi[j,t]/sum(xi[j,t:K])
-
       }
       xi_tilde[K] <- 1
+      for(t in 1:(K-1)){
+        Gamma[,,t] <- make_dt_Gamma(xi_tilde[t+1], phi[j,t], zeta)
+        Pmats[,,t] <- make_dt_Pmats(p[j,t],nd)
+      }
+      Pmats[,,K] <- make_dt_Pmats(p[j,K],nd)
+      pi <- make_dt_pi(xi_tilde[1],nd)
 
-      ### Predict abundance
+      ### Predict abundance for uncaptured id
+      nu_t <- sample_undet_ms(nu[j], pi, Pmats, Gamma)
 
-      nu_t[1:n_states, 1:K] <- sample_undet_ms(n=nu[j], init=pi,
-                                               probObs=Pmats, probTrans=Gamma)
+      ### Predict state for captured id
       for(i in 1:n){
-        det_state[i,1:K] <- sample_det_ms(x=x, init=pi,
-                                          probObs=Pmats, probTrans=Gamma)
+        det_state[i, ] <- sample_det_ms(x[i, ], pi, Pmats, Gamma)
       }
-      nest_obs <- det_state==2
-      avail_obs[1:nobs,1:K] <- 1-((det_state[1:nobs,1:K]==1) + (det_state[1:nobs,1:K]==nd))
+      ### Determine abundnace for nesting and avaialble id
       for(t in 1:K){
-        N_nest[t] <- sum(nest_obs[1:nobs, t]) + nu_t[2,t]
-        N[t] <- nu - (nu_t[1,t]+nu_t[nd,t]) + sum(avail_obs[1:nobs, t])
+        n_nest_obs <- 0
+        n_avail_obs <- 0
+        for(i in 1:n){
+          if(det_state[i, t] == 2) n_nest_obs <- n_nest_obs + 1
+          # Check if individual is 'available' (not in state 1 or n_states)
+          if(det_state[i, t] > 1 & det_state[i, t] < n_states) {
+            n_avail_obs <- n_avail_obs + 1
+          }
+        }
+        # Assign to your result matrices
+        N_nest[j, t] <- n_nest_obs + nu_t[2, t]
+        N_avail[j, t] <-  n_avail_obs + nu[j] - (nu_t[1, t] + nu_t[n_states, t])
       }
-    } # i loop end
-  } # j loop end
+
+    } # MCMC iteration loop end (j index)
+
+    out <- predlist$new()
+    # out$zeta <- zeta
+    # out$pi <- pi
+    # out$Gamma <- Gamma
+    # out$Pmats <- Pmats
+    # out$nu_t <- nu_t
+    out$N_avail <- N_avail
+    out$N_nest <- N_nest
+    returnType(predlist())
+    return(out)
+  }) # function end
